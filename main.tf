e3e45f4b3e4c9eba192a809662f925f21ee94962
Bison <39214022+bisonjp@users.noreply.github.com>
2023-10-02 23:35:00 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,20 @@
-
+variable "vpc_name" {
+  description = "VPCの名前"
+  type        = string
+}
+# VPCタグ
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = var.vpc_name
+  }
+}
+# パブリックサブネットタグ
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "ap-northeast-1a"
+  tags = {
+    Name = "${var.vpc_name}-public-subnet"
+  }
+}
