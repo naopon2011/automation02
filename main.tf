7d6a44c62122eecab96101e5bbbece3995c215bc
Bison <39214022+bisonjp@users.noreply.github.com>
2023-10-04 15:42:03 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,3 +22,45 @@ resource "aws_subnet" "public_subnet" {
     Name = "${var.vpc_name}-public-subnet"
   }
 }
+# プライベートサブネットタグ
+resource "aws_subnet" "private_subnet" {
+  count             = 2
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.${count.index + 1}.0/24"
+  availability_zone = "ap-northeast-1a"
+  tags = {
+    Name = "${var.vpc_name}-private-subnet${count.index + 1}"
+  }
+}
+
+# インターネットゲートウェイタグ
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "${var.vpc_name}-igw"
+  }
+}
+
+# デフォルトルートをインターネットゲートウェイに設定
+resource "aws_route" "public_default_route" {
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.my_igw.id
+}
+
+# インターネットゲートウェイタグ
+resource "aws_nat_gateway" "my_nat_gateway" {
+  allocation_id = aws_eip.my_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+  tags = {
+    Name = "${var.vpc_name}-nat-gateway"
+  }
+}
+
+# Elastic IPタグ
+resource "aws_eip" "my_eip" {
+  vpc = true
+  tags = {
+    Name = "${var.vpc_name}-eip"
+  }
+}
