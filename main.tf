656597508901c017e750a1d0e7d75bff8ae0ce37
Bison <39214022+bisonjp@users.noreply.github.com>
2023-10-05 08:57:28 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -107,22 +107,22 @@ resource "aws_route_table_association" "private_subnet_association" {
 
 locals {
   command = <<EOF
-      #!/bin/bash
-      #Stop the App Connector service which was auto-started at boot time
-      systemctl stop zpa-connector
-      #Create a file from the App Connector provisioning key created in the ZPA Admin Portal
-      #Make sure that the provisioning key is between double quotes
-      echo "${var.provision_key}" > /opt/zscaler/var/provision_key
-      #Run a yum update to apply the latest patches
-      yum update -y
-      #Start the App Connector service to enroll it in the ZPA cloud
-      systemctl start zpa-connector
-      #Wait for the App Connector to download latest build
-      sleep 60
-      #Stop and then start the App Connector for the latest build
-      systemctl stop zpa-connector
-      systemctl start zpa-connector
-      EOF
+#!/bin/bash
+#Stop the App Connector service which was auto-started at boot time
+systemctl stop zpa-connector
+#Create a file from the App Connector provisioning key created in the ZPA Admin Portal
+#Make sure that the provisioning key is between double quotes
+echo "${var.provision_key}" > /opt/zscaler/var/provision_key
+#Run a yum update to apply the latest patches
+yum update -y
+#Start the App Connector service to enroll it in the ZPA cloud
+systemctl start zpa-connector
+#Wait for the App Connector to download latest build
+sleep 60
+#Stop and then start the App Connector for the latest build
+systemctl stop zpa-connector
+systemctl start zpa-connector
+   EOF
 }
 
 resource "aws_instance" "example" {
