fa456f5248480fa2817b8ebec48ef95e705b5d2d
Bison <39214022+bisonjp@users.noreply.github.com>
2023-10-04 20:40:12 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ resource "aws_route" "public_default_route" {
 }
 
 #NATゲートウェイタグ
-resource "aws_nat_gateway" "my_nat_gateway" {
+resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.eip.id
   subnet_id     = aws_subnet.public_subnet.id
   tags = {
@@ -90,6 +90,19 @@ resource "aws_eip" "eip" {
 resource "aws_route_table_association" "public_subnet_association" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_association" {
+  subnet_id      = aws_subnet.private_subnet1.id
+  route_table_id = aws_route_table.private_route_table.id
 }
 
 resource "aws_instance" "example" {
