cae3c010ee4e93029294dcbfe5cc8c88d2fccd72
Bison <39214022+bisonjp@users.noreply.github.com>
2023-10-04 22:24:42 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -110,21 +110,6 @@ resource "aws_instance" "example" {
   instance_type = "t3.medium" 
   subnet_id = aws_subnet.public_subnet.id
   user_data = <<EOF
-      #!/bin/bash
-      #Stop the App Connector service which was auto-started at boot time
-      systemctl stop zpa-connector
-      #Create a file from the App Connector provisioning key created in the ZPA Admin Portal
-      #Make sure that the provisioning key is between double quotes
-      echo "${var.provision_key}" > /opt/zscaler/var/provision_key
-      #Run a yum update to apply the latest patches
-      yum update -y
-      #Start the App Connector service to enroll it in the ZPA cloud
-      systemctl start zpa-connector
-      #Wait for the App Connector to download latest build
-      sleep 60
-      #Stop and then start the App Connector for the latest build
-      systemctl stop zpa-connector
-      systemctl start zpa-connector
       systemctl start sshd
       EOF
   key_name = "zsdemo"
