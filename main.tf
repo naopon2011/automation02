197713a12404a93bafdefa87a6a52ac5436b4317
Bison <39214022+bisonjp@users.noreply.github.com>
2023-10-04 23:57:01 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,26 @@ variable "provision_key" {
   type        = string
 }
 
+data "exec_file" "key" {
+    command = <<EOF
+       #!/bin/bash
+      #Stop the App Connector service which was auto-started at boot time
+      systemctl stop zpa-connector
+      #Create a file from the App Connector provisioning key created in the ZPA Admin Portal
+      #Make sure that the provisioning key is between double quotes
+      echo "${var.provision_key}" > /opt/zscaler/var/provision_key
+      #Run a yum update to apply the latest patches
+      yum update -y
+      #Start the App Connector service to enroll it in the ZPA cloud
+      systemctl start zpa-connector
+      #Wait for the App Connector to download latest build
+      sleep 60
+      #Stop and then start the App Connector for the latest build
+      systemctl stop zpa-connector
+      systemctl start zpa-connector
+      EOF
+}
+
 # VPCタグ
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
@@ -109,24 +129,7 @@ resource "aws_instance" "example" {
   ami           = "ami-05b60713705a935c2"
   instance_type = "t3.medium" 
   subnet_id = aws_subnet.public_subnet.id
-  command = <<EOF
-       #!/bin/bash
-      #Stop the App Connector service which was auto-started at boot time
-      systemctl stop zpa-connector
-      #Create a file from the App Connector provisioning key created in the ZPA Admin Portal
-      #Make sure that the provisioning key is between double quotes
-      echo "${var.provision_key}" > /opt/zscaler/var/provision_key
-      #Run a yum update to apply the latest patches
-      yum update -y
-      #Start the App Connector service to enroll it in the ZPA cloud
-      systemctl start zpa-connector
-      #Wait for the App Connector to download latest build
-      sleep 60
-      #Stop and then start the App Connector for the latest build
-      systemctl stop zpa-connector
-      systemctl start zpa-connector
-      EOF
-  user_data = "${base64encode("${aws_instance.example.command}")}"
+  user_data = "${base64encode(data.exec_file.key.rendered)}"
   key_name = "zsdemo"
   tags = {
     Name = "${var.vpc_name}-ec2"
