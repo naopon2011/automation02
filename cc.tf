889a9f98ec3b782932d0c2c223d3ff23a592f394
Bison <39214022+bisonjp@users.noreply.github.com>
2023-10-05 17:35:33 +0900

    Update cc.tf

--- cc.tf
+++ cc.tf
@@ -7,8 +7,8 @@ resource "aws_instance" "cc_vm" {
   instance_type               = t3.medium
  # iam_instance_profile        = element(var.iam_instance_profile, count.index)
  # vpc_security_group_ids      = [element(var.mgmt_security_group_id, count.index)]
-  subnet_id                   = element(var.mgmt_subnet_id, count.index)
-  key_name                    = var.instance_key
+  subnet_id                   = aws_subnet.private_subnet1.id
+  key_name                    = "zsdemo"
   associate_public_ip_address = false
   user_data                   = base64encode(var.user_data)
 
