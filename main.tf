55a79f6f973321857e48e2ab6d2412b3a354a5b6
Bison <39214022+bisonjp@users.noreply.github.com>
2023-10-05 09:31:55 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    zpa = {
+      source  = "zscaler.com/zpa/zpa"
+      version = "2.2.2"
+    }
+  }
+}
+provider "zpa" {}
+
+data "zpa_app_connector_controller" "example" {
+  name = "AWS-VPC100-App-Connector"
+}
+
+output "zpa_app_connector_controller" {
+  value = data.zpa_app_connector_controller.example
+}
+
 provider "aws" {
   region = "ap-northeast-1"
 }
