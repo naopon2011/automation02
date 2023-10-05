8d788df40201a20af7f0df49a0e9c947882d8b47
Bison <39214022+bisonjp@users.noreply.github.com>
2023-10-05 20:31:12 +0900

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,15 @@
+variable "name_prefix" {
+  type        = string
+  description = "The name prefix for all your resources"
+  default     = "zscc"
+  validation {
+    condition     = length(var.name_prefix) <= 12
+    error_message = "Variable name_prefix must be 12 or less characters."
+  }
+}
 
+variable "cc_callhome_enabled" {
+  type        = bool
+  description = "determine whether or not to create the cc-callhome-policy IAM Policy and attach it to the CC IAM Role"
+  default     = true
+}
