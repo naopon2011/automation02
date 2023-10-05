d233ae1601a05acece059097915d04dbc20fc45c
Bison <39214022+bisonjp@users.noreply.github.com>
2023-10-05 23:13:44 +0900

    Update cc.tf

--- cc.tf
+++ cc.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "cc_vm" {
   subnet_id                   = aws_subnet.private_subnet1.id
   key_name                    = "zsdemo"
   associate_public_ip_address = false
-  user_data                   = base64encode(local.cc_command)
+  user_data                   = base64encode(local.user_data)
 
   metadata_options {
     http_endpoint = "enabled"
