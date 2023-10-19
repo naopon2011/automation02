af6d3ccedddfe2edefb057d8d78dc4870103424a
Bison <39214022+bisonjp@users.noreply.github.com>
2023-10-19 11:57:23 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -98,26 +98,6 @@ resource "aws_route_table_association" "private_subnet_association" {
   route_table_id = aws_route_table.private_route_table.id
 }
 
-locals {
-  command = <<EOF
-#!/bin/bash
-#Stop the App Connector service which was auto-started at boot time
-systemctl stop zpa-connector
-#Create a file from the App Connector provisioning key created in the ZPA Admin Portal
-#Make sure that the provisioning key is between double quotes
-echo "${var.provision_key}" > /opt/zscaler/var/provision_key
-#Run a yum update to apply the latest patches
-yum update -y
-#Start the App Connector service to enroll it in the ZPA cloud
-systemctl start zpa-connector
-#Wait for the App Connector to download latest build
-sleep 60
-#Stop and then start the App Connector for the latest build
-systemctl stop zpa-connector
-systemctl start zpa-connector
-   EOF
-}
-
 resource "aws_security_group" "sg" {
   # ... other configuration ...
 
@@ -126,17 +106,6 @@ resource "aws_security_group" "sg" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "app_connector" {
-  ami           = "ami-05b60713705a935c2"
-  instance_type = "t3.medium" 
-  subnet_id = aws_subnet.private_subnet1.id
-  user_data = base64encode(local.command)
-  key_name = "zsdemo"
-  tags = {
-    Name = "${var.vpc_name}-ec2"
   }
 }
 
