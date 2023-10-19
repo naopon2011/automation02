24ae6d92939fd11b884242defc8b6ef6995375da
Bison <39214022+bisonjp@users.noreply.github.com>
2023-10-19 12:17:29 +0900

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
-output "security_group" {
-  value       = aws_security_group.sg.id
-}
-output "private_subnet" {
-  value       = aws_subnet.public_subnet.id
-}
+# output "security_group" {
+#  value       = aws_security_group.sg.id
+# }
+# output "private_subnet" {
+#  value       = aws_subnet.public_subnet.id
+# }
