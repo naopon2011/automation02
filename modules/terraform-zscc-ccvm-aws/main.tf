b32f746befc7d7aedc1f8f34592ddf3d18cbd559
Bison <39214022+bisonjp@users.noreply.github.com>
2023-10-19 16:44:41 +0900

    Update main.tf

--- modules/terraform-zscc-ccvm-aws/main.tf
+++ modules/terraform-zscc-ccvm-aws/main.tf
@@ -23,9 +23,6 @@ resource "aws_instance" "cc_vm" {
   key_name                    = var.instance_key
   associate_public_ip_address = false
   user_data                   = base64encode(var.user_data)
-　#　　　tags = {
-#    Name = "${var.vpc_name}-cc"
-#  }
 
   metadata_options {
     http_endpoint = "enabled"
