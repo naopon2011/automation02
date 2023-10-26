b44331db38a47389160a33e5e10b409e7c6fa68e
Bison <39214022+bisonjp@users.noreply.github.com>
2023-10-26 11:23:30 +0900

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "windows_hostname" {
   description = "EC2(Windows) hostname for accessing via ZPA"
-  value       = aws_instance.bastion.public_ip
+  value       = aws_instance.windows.private_dns
 }
