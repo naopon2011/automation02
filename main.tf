3a9456db1c65ecaeba5b08aa66e5809a0e95a635
Bison <39214022+bisonjp@users.noreply.github.com>
2023-10-26 11:30:01 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -134,14 +134,18 @@ resource "aws_route_table_association" "private_subnet_association2" {
 }
 
 resource "aws_security_group" "sg" {
-  # ... other configuration ...
-
+  vpc_id = aws_vpc.vpc.id
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
   }
+  tags = {
+    Name = "${var.vpc_name}-sg"
+    Tag = var.vpc_name
+  }
+
 }
 
 #Windows Serverの作成
