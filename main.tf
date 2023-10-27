f86408a4a02c3c5e8c27a1893580c5956d9fd6f3
Bison <39214022+bisonjp@users.noreply.github.com>
2023-10-27 16:43:19 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 # VPCの作成
 resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
   enable_dns_hostnames = true
   tags = {
     Name = var.vpc_name,
